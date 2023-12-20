aa5ae23ed7b70e6e9aaff5b0eb0ec6c5092a2705
Галкин Дмитрий <BleyOgarSteam@yandex.ru>
2023-12-20 21:44:51 +0300

    Реализован сервис с репозиториями и хелпером

--- app/src/main/java/com/example/lab_3/App.java
+++ app/src/main/java/com/example/lab_3/App.java
@@ -3,7 +3,7 @@ package com.example.lab_3;
 import android.app.Application;
 
 import com.example.lab_3.service.CommonService;
-import com.example.lab_3.service.GroupMatesHelper;
+import com.example.lab_3.service.Service;
 
 public class App extends Application {
     @Override
@@ -14,6 +14,6 @@ public class App extends Application {
 
     private void initializeServices() {
         CommonService.createInstance(getApplicationContext());
-        GroupMatesHelper.createInstance(getApplicationContext());
+        Service.createInstance(getApplicationContext());
     }
 }
--- app/src/main/java/com/example/lab_3/service/GroupMatesHelper.java
+++ app/src/main/java/com/example/lab_3/service/GroupMatesHelper.java
@@ -4,36 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.lab_3.service.impl.SQLiteGroupMatesImpl;
-import com.example.lab_3.service.repository.GroupMatesRepository;
-
 public class GroupMatesHelper extends SQLiteOpenHelper {
-    private static GroupMatesHelper instance;
     public static final String DB_NAME = "lab3_db";
     public static final int DB_VERSION = 1;
-    public GroupMatesRepository groupMatesRepository;
 
-    public static GroupMatesHelper getInstance() {
-        return instance;
-    }
-
-    public static GroupMatesHelper createInstance(Context context) {
-        if (instance == null) instance = new GroupMatesHelper(context);
-        return instance;
-    }
 
     public GroupMatesHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
-        this.groupMatesRepository = new SQLiteGroupMatesImpl(this);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        groupMatesRepository.onCreate(db);
+        Service.getInstance().groupMatesRepository.onCreate(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        groupMatesRepository.onUpgrade(db, oldVersion, newVersion);
+        Service.getInstance().groupMatesRepository.onUpgrade(db, oldVersion, newVersion);
     }
 }
